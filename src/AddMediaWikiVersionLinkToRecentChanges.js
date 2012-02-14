b331cd025fecdf413559c79f1ba97e4fd7dce9fa
Helder <he7d3r@gmail.com>
2012-02-14 17:55:05 +0000

    Add link to the log of changes to the current MediaWiki version on Special:Recentchanges
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/AddMediaWikiVersionLinkToRecentChanges.js
@@ -0,0 +1,48 @@
+if ( 'Recentchanges' == mw.config.get( 'wgCanonicalSpecialPageName' ) ) {
+    $.getJSON(
+	mw.util.wikiScript( 'api' ), {
+			'format': 'json',
+			'action': 'query',
+			'meta': 'siteinfo'
+		}, function(data) {
+			if( data && data.query ) {
+				var	geral = data.query.general,
+					curRev = geral.rev,
+					oldRev = $.cookie( 'mw-last-checked-rev' ) || 99292, // Last checked revision
+					HTML = geral.generator + ': r' + curRev,
+					branch = geral.generator.match( /MediaWiki (.+)/ )[ 1 ];
+				mw.util.addCSS(
+					'#my-mw-version { z-index:1; font-size:75%; position:absolute; top: 2px; left:2px; }' +
+					'#my-mw-version a.updated { color:green; font-weight:bold; }'
+				);
+				var	$div = $( '<div id="my-mw-version"></div>' ),
+					$versionLink = $( '<a>' + HTML + '</a>' ).attr( {
+						'href': '//www.mediawiki.org/wiki/Special:Code/MediaWiki?path=%2Fbranches%2Fwmf%2F' + branch,
+						'class': 'updated',
+						'title': 'Ver as alterações recentes no branch /wmf/' + branch + '.'
+					} ),
+					$okLink = $( '<a>ok</a>' )
+						.attr( 'href', '//pt.wikibooks.org/wiki/User:Helder.wiki/common.js?action=edit' )
+						.click(function(e){
+							e.preventDefault();
+							$.cookie( 'mw-last-checked-rev', curRev, {
+								expires: 30,
+								path: '/'
+							} );
+							document.location.reload( false ); // Reloads the document (from the cache)
+						}),
+					$normalLink = $('<a>' + HTML + '</a>' )
+						.attr( 'href', mw.util.wikiGetlink( 'Special:Version' ) );
+				if ( oldRev < curRev ) {
+					$div.append( $versionLink )
+						.append( ' ( ' )
+						.append( $okLink )
+						.append( ' )' );
+				} else {
+					$div.append( $normalLink );
+				}
+				$div.appendTo( '#mw-head' );
+			}
+		}
+	);
+}
